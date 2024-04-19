d2d5a34303c2e1e2e333fb666db9f881fa67f604
Sohail Akhtar Ali <sohail@truscholar.io>
2024-04-19 19:09:07 +0530

    Sohail | Feat: Added Schema & Models Implementing Zod

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -6,9 +6,10 @@ export const usernameValidation = z
   .max(20, "Username must be not be more than 20 characters")
   .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special character");
 
-
-  export const signUpSchema = z.object({
-    username: usernameValidation,
-    email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(6, { message: "password must be atleast 6 characters" })
-  })
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be atleast 6 characters" }),
+});
