cb5cec04f891155d60ecfec90fee940380a43bec
Sohail Akhtar Ali <sohail@truscholar.io>
2024-04-20 14:01:03 +0530

    Sohail | Fix: Implementing the NextAuth feature in Web

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,97 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import UserModal from "@/model/User";
+import bcrypt, { hash } from "bcryptjs";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+    const existingVerifiedUserByUsername = await UserModal.findOne({
+      username,
+      isVerifiedL: true,
+    });
+
+    if (existingVerifiedUserByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken!",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUserByEmail = await UserModal.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with ",
+          },
+          { status: 400 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now(), +3600000);
+
+        await existingUserByEmail.save();
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModal({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        isVerified: false,
+        verifyCodeExpiry: expiryDate,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: false,
+        message: "User registered successfully. please verify your mail.",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("Error regestering User", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error regestering user..",
+      },
+      { status: 500 }
+    );
+  }
+}
