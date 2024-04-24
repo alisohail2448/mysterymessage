bb8fe90c09f3087c871d1ebb8edf0a71a225d37d
Sohail Akhtar Ali <sohail@truscholar.io>
2024-04-24 18:18:50 +0530

    Sohail | Feat: Implemented Next Auth and Api Endpoints

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,69 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModal from "@/model/User";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { z } from "zod";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+    const result = UsernameQuerySchema.safeParse(queryParam);
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid query parameters",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModal.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (!existingVerifiedUser) {
+      return Response.json(
+        {
+          success: true,
+          message: "Username is unique",
+        },
+        { status: 200 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: false,
+        message: "Username is already taken",
+      },
+      { status: 400 }
+    );
+  } catch (error) {
+    console.error("Error in checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in checking username",
+      },
+      { status: 500 }
+    );
+  }
+}
